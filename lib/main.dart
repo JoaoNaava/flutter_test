9bdd4c7b1db1e521d4f2a178bbefcf074610cf54
JoaoNaava <joaopedronava9@gmail.com>
2026-03-23 21:41:07 -0300

    Teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
-
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Text("teste")
-      )
-    )
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(225, 176, 102, 189),
+          title: Center(
+            child: Text("Nome do meu app"),
+          ),
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Olá mundo!"),
+              Text("23/03/2026"),
+            ],
+          ),
+        ),
+      ),
+    ),
   );
 }
